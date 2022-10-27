b4328b605bd91317a81268be7b427133efb3344c
aallen23 <allenanna917@gmail.com>
2022-10-27 14:44:26 -0400

    updating menu

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,21 +9,28 @@ public class UIManager : MonoBehaviour
     public GameObject ui;
     public GameObject endGame;
 
+    public GameObject healthBar;
+
     public TMP_Text score;
-    public TMP_Text health;
     public TMP_Text finalScore;
+
+    public int health;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        health.text = "Health: " + StaticGameClass.health;
-        score.text = "Score: " + StaticGameClass.score;
+        score.text = "" + StaticGameClass.score;
+    }
+
+    public void UpdateHealth()
+    {
+        healthBar.transform.GetChild(StaticGameClass.health).gameObject.SetActive(false);
     }
 
     public void EnableHome()
